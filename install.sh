514900668c78e2ec676038b3cd074ed19eca433c
Sekky61 <misa@majer.cz>
2021-08-22 10:34:44 -0700

    vscode, bat link, sudo

--- install.sh
+++ install.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo "Updating and Upgrading"
-apt-get update && sudo apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
-#gcc
-apt install gcc
+# apt install of common programs
+echo "Installing common programs"
+apt-get install gcc curl git gnome-tweaks -y
 
-#curl
-apt install curl
-
-#Install git
-echo "Installing Git, please congiure git later..."
-apt install git -y
+# VS code
+snap install --classic code
+#todo apply settings, extensions
 
 #Chrome
 echo "Installing Google Chrome"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get update 
 apt-get install google-chrome-stable -y
+
+# bat
+echo -n "Install bat here -> "
+curl --silent "https://github.com/sharkdp/bat/releases/latest" |
+    grep 'href=' |                                              
+    sed -E 's/.*"([^"]+)".*/\1/'
+    
