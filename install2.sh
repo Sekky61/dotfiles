713ba89e8bb0d83a3818c3ef57dea228e8032ba2
Michal Majer <misa@majer.cz>
2022-06-17 15:06:42 +0200

    mold

--- install2.sh
+++ install2.sh
@@ -2,10 +2,17 @@
 ##  brew
 ##
 
-# todo no sudo
+# todo sudo
 
 echo $USER
 
 nvm install node
+
+echo "Installing mold"
+git clone https://github.com/rui314/mold.git
+cd mold
+git checkout v1.2.1
+make -j$(nproc) CXX=g++
+make install
 
 echo "Done. Restart shell."
