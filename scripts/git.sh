d536037c54ab00fd101bad1117e33157307140a4
Michal Majer <misa@majer.cz>
2023-02-16 20:08:27 +0100

    testing gpg signed commit

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,3 +4,11 @@
 apt-get install git -y
 
 # todo download git-credential-manager
+
+# todo setup git to use gpg key
+# git config --global user.signingkey <key>
+
+# sign by default
+# git config --global commit.gpgsign true
+
+# git commit with -S flag to sign commit
